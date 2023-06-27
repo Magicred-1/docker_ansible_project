c74fbab3646d8e143a3116c3ed837f5bb65c48d1
Djason Gadiou <86433099+Magicred-1@users.noreply.github.com>
2023-06-27 15:45:16 +0200

    Adjusting API and Dockerfile for deployment

---
+++ docker_images/WEB/files/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,12 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\Djason Gadiou\\Documents\\GitHub\\docker_ansible_project\\docker_images\\web\\files\\src\\app\\layout.tsx": [
+      "C:\\Users\\Djason Gadiou\\Documents\\GitHub\\docker_ansible_project\\docker_images\\web\\files\\src\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\Djason Gadiou\\Documents\\GitHub\\docker_ansible_project\\docker_images\\web\\files\\src\\app\\page": [
+      "C:\\Users\\Djason Gadiou\\Documents\\GitHub\\docker_ansible_project\\docker_images\\web\\files\\src\\app\\globals.css"
+    ]
+  }
+}
